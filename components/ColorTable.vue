fd25425052b1a326f7dd268850de8bda64bba2c9
reireias <reireias@gmail.com>
2019-08-22 22:13:33 +0900

    update result table

---
+++ components/ColorTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <table class="color-table">
+    <tr>
+      <td></td>
+      <td v-for="(label, index) in xlabels" :key="index">{{ label }}</td>
+    </tr>
+    <tr v-for="(row, index) in data" :key="index">
+      <td>{{ ylabels[index] }}</td>
+      <td v-for="(value, index2) in row" :key="index2">{{ value }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    xlabels: {
+      type: Array,
+      required: true
+    },
+    ylabels: {
+      type: Array,
+      required: true
+    },
+    data: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.color-table {
+  border: solid 1px;
+  border-collapse: collapse;
+}
+.color-table td {
+  border: solid 1px;
+  text-align: center;
+  padding: 10px;
+}
+@media screen and (min-width: 0px) and (max-device-width: 640px) {
+  .color-table td {
+    font-size: 10px;
+    padding: 2px;
+  }
+}
+</style>
--- pages/power-or-critical.vue
+++ pages/power-or-critical.vue
@@ -8,74 +8,97 @@
       suffix="%"
     ></v-text-field>
     <v-btn color="primary" @click="calculate">計算</v-btn>
-    <heat-map v-if="series" :series="series" :min="min" :max="max"></heat-map>
+    <div class="result-box">
+      <center>
+        <color-table
+          v-if="data"
+          :xlabels="attackLabels"
+          :ylabels="criticalLabels"
+          :data="data"
+        ></color-table>
+      </center>
+    </div>
   </div>
 </template>
 
 <script>
-import HeatMap from '@/components/HeatMap'
+import ColorTable from '@/components/ColorTable'
+import { calculateExpectedValue } from '@/utils/expected'
 export default {
   components: {
-    HeatMap
+    ColorTable
   },
   data() {
     return {
       base: 200,
       critical: 0,
       attackSkills: [
-        { name: '攻撃Lv0', attack: 0, critical: 0 },
-        { name: '攻撃Lv1', attack: 3, critical: 0 },
-        { name: '攻撃Lv2', attack: 6, critical: 0 },
-        { name: '攻撃Lv3', attack: 9, critical: 0 },
-        { name: '攻撃Lv4', attack: 12, critical: 5 },
-        { name: '攻撃Lv5', attack: 15, critical: 5 },
-        { name: '攻撃Lv6', attack: 18, critical: 5 },
-        { name: '攻撃Lv7', attack: 21, critical: 5 }
+        { attack: 0, critical: 0 },
+        { attack: 3, critical: 0 },
+        { attack: 6, critical: 0 },
+        { attack: 9, critical: 0 },
+        { attack: 12, critical: 5 },
+        { attack: 15, critical: 5 },
+        { attack: 18, critical: 5 },
+        { attack: 21, critical: 5 }
+      ],
+      attackLabels: [
+        '攻撃Lv0',
+        '攻撃Lv1',
+        '攻撃Lv2',
+        '攻撃Lv3',
+        '攻撃Lv4',
+        '攻撃Lv5',
+        '攻撃Lv6',
+        '攻撃Lv7'
       ],
       criticalSkills: [
-        { name: '見切りLv0', critical: 0 },
-        { name: '見切りLv1', critical: 3 },
-        { name: '見切りLv2', critical: 6 },
-        { name: '見切りLv3', critical: 10 },
-        { name: '見切りLv4', critical: 15 },
-        { name: '見切りLv5', critical: 20 },
-        { name: '見切りLv6', critical: 25 },
-        { name: '見切りLv7', critical: 30 }
+        { critical: 0 },
+        { critical: 3 },
+        { critical: 6 },
+        { critical: 10 },
+        { critical: 15 },
+        { critical: 20 },
+        { critical: 25 },
+        { critical: 30 }
       ],
-      series: null,
-      min: 0,
-      max: 999
+      criticalLabels: [
+        '見切りLv0',
+        '見切りLv1',
+        '見切りLv2',
+        '見切りLv3',
+        '見切りLv4',
+        '見切りLv5',
+        '見切りLv6',
+        '見切りLv7'
+      ],
+      data: null
     }
   },
   methods: {
     calculate() {
-      this.setSeries()
-    },
-    setSeries() {
-      this.series = null
-      this.base = Number(this.base)
-      this.critical = Number(this.critical)
-      const series = this.criticalSkills.map((criticalSkill) => {
-        const obj = { name: criticalSkill.name }
-        obj.data = this.attackSkills.map((attackSkill) => {
-          const currentCritical =
-            this.critical + criticalSkill.critical + attackSkill.critical
-          const currentAttack = this.base + attackSkill.attack
-          const expect =
-            (currentAttack *
-              (1.25 * currentCritical + (100 - currentCritical))) /
-            100
-          return {
-            x: attackSkill.name,
-            y: expect
-          }
+      const data = []
+      this.criticalSkills.forEach((criticalSkill) => {
+        const row = []
+        this.attackSkills.forEach((attackSkill) => {
+          const attack = Number(this.base) + attackSkill.attack
+          const critical =
+            Number(this.critical) +
+            attackSkill.critical +
+            criticalSkill.critical
+          const expected = calculateExpectedValue(attack, critical, 1.25)
+          row.push((Math.round(expected * 100) / 100).toFixed(2))
         })
-        return obj
+        data.push(row)
       })
-      this.min = series[0].data[0].y
-      this.max = series[7].data[7].y
-      this.series = series
+      this.data = data
     }
   }
 }
 </script>
+
+<style>
+.result-box {
+  margin-top: 10px;
+}
+</style>
