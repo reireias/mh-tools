5b5241afbef5d8f5e985a6b7547f8b6037158045
reireias <reireias@gmail.com>
2019-08-25 06:37:02 +0900

    add top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-row justify="center">
+      <h1 class="primary--text">MH Tools</h1>
+    </v-row>
+    <v-row justify="center">
+      <nuxt-link to="/power-or-critical">攻撃 or 見切り</nuxt-link>
+    </v-row>
+  </v-container>
 </template>
