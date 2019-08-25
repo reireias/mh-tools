833eb0fe41f1ddffe34dd6d66bfe5bcac7c9363d
reireias <reireias@gmail.com>
2019-08-25 13:22:50 +0900

    add components test

---
+++ test/components/ColorTable.test.js
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils'
+import ColorTable from '@/components/ColorTable'
+
+describe('ColorTable', () => {
+  const props = {
+    xlabels: ['A', 'B', 'C'],
+    ylabels: ['X', 'Y', 'Z'],
+    data: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  }
+
+  it('should be vue instance', () => {
+    const wrapper = mount(ColorTable, {
+      propsData: props
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  describe('maxValues', () => {
+    it('should be max value array', () => {
+      const wrapper = mount(ColorTable, {
+        propsData: props
+      })
+      expect(wrapper.vm.maxValues).toStrictEqual([1, 4, 7, 8, 9])
+    })
+  })
+})
