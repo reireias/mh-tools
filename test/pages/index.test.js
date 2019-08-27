d94f07f55cfa7c535800372353cfbae4559c178a
reireias <reireias@gmail.com>
2019-08-27 11:19:41 +0900

    add index test

---
+++ test/pages/index.test.js
@@ -0,0 +1,23 @@
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import target from '@/pages/index'
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+
+describe('power-or-critical', () => {
+  it('should be vue instance', () => {
+    const vuetify = new Vuetify()
+    const wrapper = mount(target, {
+      localVue,
+      vuetify,
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
